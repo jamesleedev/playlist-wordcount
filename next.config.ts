e755ab12003798886f038e6ff73c7537b717240c
James Lee <james@jamesl.dev>
2025-03-25 02:16:43 +0000

    chore: add bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ['@remixicon/react', 'lucide-react'],
+  },
 };
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
+
+export default bundleAnalyzer(nextConfig);
