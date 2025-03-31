51e697a37ad158634c58d751c341faa5c66acc93
James Lee <james@jamesl.dev>
2025-03-31 12:43:55 +0100

    ci: update start docker script with project name

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 LATEST_TAG=$(git describe --tags --abbrev=0)
-IMAGE="homepage:${LATEST_TAG}"
+IMAGE="pwc:${LATEST_TAG}"
 
-echo "##### Building homepage:${LATEST_TAG} #####"
+echo "##### Building pwc:${LATEST_TAG} #####"
 
 sudo docker build -t $IMAGE .
 
-echo "##### Removing existing homepage container #####"
-sudo docker rm -f homepage 2> /dev/null || true
+echo "##### Removing existing pwc container #####"
+sudo docker rm -f pwc 2> /dev/null || true
 
-echo "##### Running homepage:${LATEST_TAG} #####"
-sudo docker run --restart unless-stopped --name "homepage" -d $IMAGE
+echo "##### Running pwc:${LATEST_TAG} #####"
+sudo docker run --restart unless-stopped --name "pwc" -d $IMAGE
